1f6094d83b06a7b440a03b0c4e671d026d02ce78
sunisaservais <sunisaservais@gmaill.com>
2026-04-06 16:56:11 -0400

    US449_VisitManagement AC01,AC02,AC03 completed

--- src/test/java/com/healthcare/step_definitions/US449_VisitManagement.java
+++ src/test/java/com/healthcare/step_definitions/US449_VisitManagement.java
@@ -1,110 +1,153 @@
 package com.healthcare.step_definitions;
 
+import com.healthcare.pages.DashboardPage;
+import com.healthcare.pages.FindPatientRecord;
+import com.healthcare.pages.RegisterAPatient;
+import com.healthcare.utilities.BrowserUtils;
+import com.healthcare.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US449_VisitManagement {
 
+    WebDriverWait wait;
+    DashboardPage dashboardPage;
+    RegisterAPatient registerAPatient;
+    FindPatientRecord findPatientRecord;
+
     @Given("user is register a patient")
     public void user_is_register_a_patient() {
-        // Write code here that turns the phrase above into concrete actions
+        dashboardPage = new DashboardPage();
+        registerAPatient = new RegisterAPatient();
 
+        dashboardPage.registerAPatientModule.click();
+        registerAPatient.checkboxUnknownPatient.click();
+
+        Select genderDropdown = new Select(Driver.getDriver().findElement(By.xpath("//select[@id='gender-field']")));
+        genderDropdown.selectByValue("F");
+
+        registerAPatient.nextButton.click();
+        BrowserUtils.waitFor(1);
+        registerAPatient.confirmButton.click();
+        BrowserUtils.waitFor(1);
+        registerAPatient.homeButton.click();
     }
 
     @Given("the user navigates to the visits page")
     public void the_user_navigates_to_the_visits_page() {
-        // Write code here that turns the phrase above into concrete actions
-
+        dashboardPage = new DashboardPage();
+        registerAPatient = new RegisterAPatient();
+        registerAPatient.homeButton.click();
+        dashboardPage.findPatientRecordModule.click();
     }
 
     @When("the user clicks on the patient name")
     public void the_user_clicks_on_the_patient_name() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.recentPatient.click();
     }
 
     @When("the user clicks on the start Visit button")
     public void the_user_clicks_on_the_start_visit_button() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.startVisitButton.click();
     }
 
     @When("the user clicks on the confirm button")
     public void the_user_clicks_on_the_confirm_button() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.startVisitConfirmButton.click();
     }
 
     @Then("the visit should be started successfully")
     public void the_visit_should_be_started_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-
+        wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        WebElement toast = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//div[contains(@class,'toast-item')]//p")
+                )
+        );
+        wait.until(ExpectedConditions.textToBePresentInElement(toast, "started a visit"));
+        Assert.assertTrue(toast.getText().contains("started a visit"));
     }
 
     @When("the user clicks on the recent visit")
     public void the_user_clicks_on_the_recent_visit() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.recentVisits.click();
     }
 
     @When("the user clicks on the actions dropdown")
     public void the_user_clicks_on_the_actions_dropdown() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.actionButton.click();
     }
 
     @Then("the user should see exactly {int} options under the actions dropdown")
-    public void the_user_should_see_exactly_options_under_the_actions_dropdown(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
-
-    @When("the user clicks on the active visit")
-    public void the_user_clicks_on_the_active_visit() {
-        // Write code here that turns the phrase above into concrete actions
-
+    public void the_user_should_see_exactly_options_under_the_actions_dropdown(int expectedActionOptions) {
+        findPatientRecord = new FindPatientRecord();
+        wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfAllElements(findPatientRecord.actionOptions));
+        Assert.assertEquals(7, findPatientRecord.actionOptions.size());
     }
 
     @When("the user clicks on the end visit button")
     public void the_user_clicks_on_the_end_visit_button() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.endVisit.click();
     }
 
     @When("the user confirms the end visit action")
     public void the_user_confirms_the_end_visit_action() {
-        // Write code here that turns the phrase above into concrete actions
-
+        wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        By yesButton = By.xpath("//div[@id='end-visit-dialog']//button[contains(@class,'confirm') and normalize-space()='Yes']");
+        WebElement yesBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(yesButton));
+        wait.until(ExpectedConditions.elementToBeClickable(yesButton)).click();
     }
 
     @Then("the visit should be ended successfully")
     public void the_visit_should_be_ended_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        Assert.assertTrue(findPatientRecord.noActiveVisit.isDisplayed());
     }
 
     @When("the user clicks on their profile name")
     public void the_user_clicks_on_their_profile_name() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.patientNameProfile.click();
     }
 
     @When("the user deletes the patient profile")
     public void the_user_deletes_the_patient_profile() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.deletePatient.click();
     }
 
     @When("the user provides a reason and confirms the deletion")
     public void the_user_provides_a_reason_and_confirms_the_deletion() {
-        // Write code here that turns the phrase above into concrete actions
-
+        findPatientRecord = new FindPatientRecord();
+        findPatientRecord.deleteReason.sendKeys("Test Reason");
+        BrowserUtils.waitFor(1);
+        findPatientRecord.reasonConfirmButton.click();
     }
 
     @Then("the patient should be deleted successfully")
     public void the_patient_should_be_deleted_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-
+        wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        WebElement toast = wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//div[contains(@class,'toast-item')]//p")
+                )
+        );
+        Assert.assertTrue(toast.getText().contains("Patient has been deleted successfully"));
     }
 }
--- src/test/resources/features/US449_VisitManagement.feature
+++ src/test/resources/features/US449_VisitManagement.feature
@@ -1,4 +1,4 @@
-@ORION-449 @wip
+@ORION-449
 Feature: Visit Management
   As a user, I should be able to start and end visits
   so that I can manage visits successfully
@@ -6,7 +6,7 @@ Feature: Visit Management
   Background:
     Given user is successfully logged in
 
-  @ORION-450 @AC1 @smoke
+  @ORION-452
   Scenario: Verify that the user is able to start a visit
     And user is register a patient
     And the user navigates to the visits page
@@ -15,7 +15,7 @@ Feature: Visit Management
     And the user clicks on the confirm button
     Then the visit should be started successfully
 
-  @ORION-451 @AC2 @regression
+  @ORION-453
   Scenario: Verify that there are 7 options under the Actions dropdown on the Visits page
     And the user navigates to the visits page
     When the user clicks on the patient name
@@ -23,11 +23,11 @@ Feature: Visit Management
     And the user clicks on the actions dropdown
     Then the user should see exactly 7 options under the actions dropdown
 
-  @ORION-452 @AC3 @regression
+  @ORION-454
   Scenario: Verify that the user is able to end an active visit
     And the user navigates to the visits page
     When the user clicks on the patient name
-    And the user clicks on the active visit
+    And the user clicks on the recent visit
     And the user clicks on the end visit button
     And the user confirms the end visit action
     Then the visit should be ended successfully
