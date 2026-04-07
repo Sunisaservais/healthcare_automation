f722eb061589f8dcc6c9a7903c3d6b9d5c45c857
sunisaservais <sunisaservais@gmaill.com>
2026-04-07 12:26:53 -0400

    Created method for unidentifiedPatient in RegisterAPatient Page and implemented in my StepDefinitions

--- src/test/java/com/healthcare/pages/RegisterAPatient.java
+++ src/test/java/com/healthcare/pages/RegisterAPatient.java
@@ -1,8 +1,11 @@
 package com.healthcare.pages;
 
+import com.healthcare.utilities.BrowserUtils;
 import com.healthcare.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class RegisterAPatient extends BasePage {
 
@@ -10,15 +13,31 @@ public class RegisterAPatient extends BasePage {
         super(Driver.getDriver());
     }
 
-    @FindBy (id = "checkbox-unknown-patient")
+    @FindBy(id = "checkbox-unknown-patient")
     public WebElement checkboxUnknownPatient;
 
-    @FindBy (id = "next-button")
+    @FindBy(id = "next-button")
     public WebElement nextButton;
 
-    @FindBy (id = "submit")
-    public  WebElement confirmButton;
+    @FindBy(id = "submit")
+    public WebElement confirmButton;
 
-    @FindBy (xpath = "//a[@href='/openmrs/referenceapplication/home.page']")
+    @FindBy(xpath = "//a[@href='/openmrs/referenceapplication/home.page']")
     public WebElement homeButton;
+
+    public void createUnidentifiedPatient(String gender) {
+        DashboardPage dashboardPage = new DashboardPage();
+        RegisterAPatient registerAPatient = new RegisterAPatient();
+        dashboardPage.registerAPatientModule.click();
+        registerAPatient.checkboxUnknownPatient.click();
+        Select genderDropdown = new Select(
+                Driver.getDriver().findElement(By.xpath("//select[@id='gender-field']"))
+        );
+        genderDropdown.selectByValue(gender);
+        registerAPatient.nextButton.click();
+        BrowserUtils.waitFor(1);
+        registerAPatient.confirmButton.click();
+        BrowserUtils.waitFor(1);
+        registerAPatient.homeButton.click();
+    }
 }
